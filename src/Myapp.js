27a1c5e8e9ce72042380f8ea03a50b650aacf32d
fatemeh nourozi <norozii.fatemeh1997@gmail.com>
2023-07-22 12:55:41 +0330

    changes on saturday

--- src/Myapp.js
+++ src/Myapp.js
@@ -3,20 +3,31 @@ import ReactDOM from "react";
 import './style.css';
 import Title from "./Title";
 import Thetime from "./Thetime";
+//////////////////////////////////////////////////////
 //function
-// start_func(() => {
+var minutes = 0;
+var seconds = 0;
+var Interval;
 
-// }
-// )
+start_func(() => {
+  clearInterval( Interval );
+  Interval = setInterval( Thetime, 999 );
+}
+)
 
-// stop_func(() => {
- 
-// }
-// )
-// reset_func(() => {
+stop_func(() => {
+  clearInterval( Interval );
+  clearInterval( Interval );
+  clearInterval( Interval );
+}
+)
+reset_func(() => {
+  clearInterval( Interval );
+  minutes = 0;
+  seconds = 0;
+}
+)
 
-// }
-// )
 //function
 
 class Myapp extends React.Component {
@@ -25,9 +36,9 @@ class Myapp extends React.Component {
       <div className='main'>
         <Title />
         <Thetime />
-        <button className="stop"  >start</button>
-        <button className="start" >stop</button>
-        <button className="reset" >reset</button>
+        <button className="stop" onClick={stop_func()} >start</button>
+        <button className="start"  onClick={start_func()}>stop</button>
+        <button className="reset"  onClick={reset_func()}>reset</button>
       </div >
     );
   }
--- src/Thetime.js
+++ src/Thetime.js
@@ -1,52 +1,68 @@
 import { clear } from "@testing-library/user-event/dist/clear";
-import React  from "react";
-import  ReactDOM from "react";
+import React from "react";
+import ReactDOM from "react";
 
 let intervall_hour;
 let intervall_minute;
 let intervall_second;
+// let intervall_hour;
+// let intervall_minute;
+// let intervall_second;
 class Thetime extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-       hour:0,
-       minute:0,
-       second:0
-      }
-    }
-
-    componentDidMount(){
-      intervall_hour= setInterval(()=>{
-        this.setState ({
-          second:this.state.second + 1 ,
-        })
-      } , 60000*60)
-
-      intervall_minute= setInterval(()=>{
-        this.setState ({
-          second:this.state.second + 1 ,
-        })
-      } ,60000)
-
-      intervall_second= setInterval(()=>{
-        this.setState ({
-          second:this.state.second + 1 ,
-        })
-      } , 1000)
-
-    }
-
-    
-    componentDidUpdate(){
-     
-    }
-    render() {
-      
-  
-      return (
-        <h1 className='Thetime'>{this.state.hour} :  {this.state.minute} : {this.state.second}</h1>
-      );
+  constructor() {
+    super();
+    this.state = {
+      hour: 0,
+      minute: 0,
+      second: 0
     }
   }
 
-  export default Thetime ;
+  componentDidMount() {
+    intervall_hour = setInterval(() => {
+      this.setState({
+        hour: this.state.hour + 1,
+      })
+    }, 60000 * 60)
+
+    intervall_minute = setInterval(() => {
+      this.setState({
+        minute: this.state.minute + 1,
+      })
+    }, 60000)
+
+    intervall_second = setInterval(() => {
+      this.setState({
+        second: this.state.second + 1,
+      })
+
+    }, 1000)
+  }
+
+  componentDidUpdate() {
+    if (this.state.second > 60) {
+      this.setState({
+        second: this.state.second = 0,
+      })
+    }
+    if (this.state.minute > 60) {
+      this.setState({
+        minute: this.state.minute = 0,
+      })
+    }
+  }
+
+  render() {
+    return (
+      <h1 className='Thetime'>{this.state.hour} :
+        {this.state.minute} :
+        {this.state.second}</h1>
+    );
+  }
+}
+
+
+
+
+
+export default Thetime;
